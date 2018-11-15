d56f3878a76fc0ab9e5ec34ecb32864ca8482024
sanjay <sanjay@deligence.com>
2018-11-15 18:31:35 +0530

    changes

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ var express = require('express');
  
 var app = express();
 
+
+app.set('port',(process.env.PORT || 8000));
+
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
  res.send('hello world');
 });
 
 //listen to port 3000 by default
-app.listen(process.env.PORT || 8000);
+app.listen(app.get('port'),function(){
+	console.log("node app is running on port " , app.get('port'))
+});
  
 module.exports = app;
